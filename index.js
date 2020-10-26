610f3abbe9c62344c51c6ebea33bb0ae776415af
Amanda Lutke <53452692+Lutkea@users.noreply.github.com>
2020-10-25 22:27:04 -0300

    CRUD concluído

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 var dados = []
+if (!JSON.parse(localStorage.getItem("__dados__") === null)) {
+    dados = JSON.parse(localStorage.getItem("__dados__"))
+}
 
 function ApagaRegistro(id) {
-    let _confirm = confirm("Deseja realmente excluir este registro?")
-
-    if (_confirm) {
+    let result = confirm("Deseja realmente excluir este registro?");
+    if (result) {
         for (let i = 0; i < dados.length; i++) {
             if (dados[i].ID == id) {
-                dados.splice(i, 1)
+                dados.splice(i, 1);
             }
         }
-
-        PopulaTabela()
-
+        PopulaTabela();
     }
 }
 
 function EditaRegistro(id) {
-    $("#modalRegistro").modal("show")
+    $("#modalRegistro").modal("show");
 
     dados.forEach(function (item) {
         if (item.ID == id) {
-            $("#hdID").val(item.ID)
-            $("#txtSabor").val(item.Sabor)
-            $("#txtTamanho").val(item.Tamanho)
-            $("#txtValor").val(item.Valor)
-            $("#txtPagamento").val(item.Pagamento)
+            $("#hdID").val(item.ID);
+            $("#txtSabor").val(item.Sabor);
+            $("#txtTamanho").val(item.Tamanho);
+            $("#txtValor").val(item.Valor);
+            $("#txtPagamento").val(item.Pagamento);
         }
     })
 }
@@ -32,72 +32,75 @@ function EditaRegistro(id) {
 function PopulaTabela() {
     if (Array.isArray(dados)) {
 
-        localStorage.setItem("__dados__", JSON.stringify(dados))
+        localStorage.setItem("__dados__", JSON.stringify(dados));
 
-        $("#tblDados tbody").html("")
+        $("#tblDados tbody").html("");
 
         dados.forEach(function (item) {
-            // template string
-            $("#tblDados tbody").append(`<tr>
-                <td>${item.ID}</td>
-                <td>${item.Sabor}</td>
-                <td>${item.Tamanho}</td>
-                <td>${item.Valor}</td>
-                <td>${item.Pagamento}</td>
-                <td><button type="button" class="btn btn-primary onclick="javascript:EditaRegistro(${Item.ID});"><i class="fa fa-edit"></button></td>
-                <td><button type="button" class="btn btn-danger" onclick="javascript:ApagaRegistro(${item.ID});"><i class="fa fa-trash"></button></td>
-                </tr>`)
-        })
+
+            $("#tblDados tbody").append(
+                `<tr>
+                    <td>${item.ID}</td>
+                    <td>${item.Sabor}</td>
+                    <td>${item.Tamanho}</td>
+                    <td>${item.Valor}</td>
+                    <td>${item.Pagamento}</td>
+                    <td>
+                        <button type="button" class="btn btn-primary" onclick="EditaRegistro(${item.ID});">
+                            <i class="fa fa-edit"></i>
+                        </button>
+                    </td>
+                    <td>
+                        <button type="button" class="btn btn-danger" onclick="ApagaRegistro(${item.ID});">
+                            <i class="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>`
+            );
+        });
     }
 }
-$(function () {
-    // armazena informações
-    dados = JSON.parse(localStorage.getItem("__dados__"))
 
-    if (dados) {
-        PopulaTabela()
-    }
+$(function () {
 
     $("#btnSalvar").click(function () {
-        // evento click do botão salvar
+        let Id = $("#hdID").val();
+        let Sabor = $("#txtSabor").val();
+        let Tamanho = $("#txtTamanho").val();
+        let Valor = $("#txtValor").val();
+        let Pagamento = $("#txtPagamento").val();
 
-        let _id = $("#hdID").val()
-        let Sabor = $("#txtSabor").val()
-        let Tamanho = $("#txtTamanho").val()
-        let Valor = $("#txtValor").val()
-        let Pagamento = $("#txtPagamento").val()
+        if (!Id || Id == "0") {
+            let registro = {
+                'Sabor': Sabor,
+                'Tamanho': Tamanho,
+                'Valor': Valor,
+                'Pagamento': Pagamento,
+                'ID': dados.length + 1
+            };
 
-        if (!_id || _id == "0") {
-            let registro = {}
-            registro.Sabor = Sabor
-            registro.Tamanho = Tamanho
-            registro.Valor = Valor
-            registro.Pagamento = Pagamento
-
-            registro.ID = dados.length + 1
-            
-            dados.push(registro)
-        }
-        else {
+            dados.push(registro);
+        } else {
             dados.forEach(function (item) {
-                if (item.ID == id) {
-                    item.Sabor = Sabor
-                    item.Tamanho = Tamanho
-                    item.Valor = Valor
-                    item.Pagamento = Pagamento
+
+                if (item.ID == Id) {
+                    item.Sabor = Sabor;
+                    item.Tamanho = Tamanho;
+                    item.Valor = Valor;
+                    item.Pagamento = Pagamento;
                 }
             })
         }
 
-        alert("Pedido salvo com sucesso!")
-        $("#modalRegistro").modal("hide")
+        alert("Pedido salvo com sucesso!");
+        $("#modalRegistro").modal("hide");
 
-        // limpa campos
-        $("#hdID").val("0")
-        $("#txtSabor").val("")
-        $("#txtTamanho").val("")
-        $("#txtValor").val("")
-        $("#txtPagamento").val("")
+
+        $("#hdID").val("0");
+        $("#txtSabor").val("");
+        $("#txtTamanho").val("");
+        $("#txtValor").val("");
+        $("#txtPagamento").val("");
 
         PopulaTabela()
     })
